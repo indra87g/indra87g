957e33452c6a94db1215d7b1248b3955c2f934eb
Indra Sah Noeldy <twinightwheel50@gmail.com>
2025-05-12 11:51:11 +0700

    feat: add readme renderer script

---
+++ index.js
@@ -0,0 +1,117 @@
+const thisYear = new Date().getFullYear()
+const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
+const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
+const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
+const progressBarOfThisYear = generateProgressBar()
+
+let monthNames = ["Jan","Feb","Mar","Apr", "May","Jun","Jul","Aug", "Sep", "Oct","Nov","Dec"];
+
+function generateProgressBar() {
+    const progressBarCapacity = 30
+    const passedProgressBarIndex = parseInt(progressOfThisYear * progressBarCapacity)
+    const progressBar = Array(progressBarCapacity)
+        .fill('▁')
+        .map((value, index) => index < passedProgressBarIndex ? '█' : value)
+        .join('')
+    return `${progressBar}`
+}
+
+console.log(`Year Progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} % as on ⏰ ${(new Date().getDate()-1)+'-'+ monthNames[new Date().getMonth()]+'-'+new Date().getFullYear()}`)
+
+const readme = `\
+<a href='https://github.com/Safouene1/support-palestine-banner'>
+  <img src='https://raw.githubusercontent.com/Safouene1/support-palestine-banner/master/banner-support.svg' alt='Support Palestine' width='100%' />
+</a>
+
+[![An image of @indra87g's Holopin badges, which is a link to view their full Holopin profile](https://holopin.me/indra87g)](https://holopin.io/@indra87g)
+
+<h1 align="center">
+  Hi there! 👋 I'm Indra Sah Noeldy
+</h1>
+
+<h3 align="center">Full Stuck Developer | Windows Enthusiast | Gamer/Modder</h3>
+
+<p align="center">
+  <a href="https://facebook.com/indra87g">
+    <img height="28" src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" />
+  </a>&nbsp;
+  <a href="https://youtube.com/indra87g">
+    <img height="28" src="https://upload.wikimedia.org/wikipedia/commons/a/a0/YouTube_social_red_circle_%282017%29.svg" />
+  </a>&nbsp;
+  <a href="https://linkedin.com/in/indra-sah-noeldy-714bb92a2"><img height="28" src="https://upload.wikimedia.org/wikipedia/commons/8/81/LinkedIn_icon.svg"></a>&nbsp;
+  <a href="https://indra87g.bsky.social"><img height="28" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Bluesky_Logo.svg/300px-Bluesky_Logo.svg.png"></a>&nbsp;
+  <a href="https://trakteer.id/indra87g" target="_blank"><img id="wse-buttons-preview" src="https://cdn.trakteer.id/images/embed/trbtn-red-1.png?date=18-11-2023" height="40" style="border:0px;height:40px;" alt="Support Me"></a>
+</p>
+
+<p align="center">
+  <img src="https://www.codewars.com/users/indra87g/badges/large" />
+</p>
+
+⏳**Year Progress:** ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} % as on ⏰ ${(new Date().getDate()-1)+'-'+ monthNames[new Date().getMonth()]+'-'+new Date().getFullYear()}
+
+### About Me 😎
+- I'm a full stuck developer from Indonesia🙏
+- I write articles on [my personal website](https://indra87g.is-a.dev)✨
+- I code in: **Python**, **Javascript**, **Golang**, **PHP**😴
+- Talk to me about Tokusatsu, Programming, IT, and Gaming👍
+- We can connect to play Minecraft🎮
+- Fun fact: nothing special
+
+### 🚀 Tech Stack  
+
+**💻 Frontend:**  
+![HTML](https://img.shields.io/badge/HTML-E34F26?style=for-the-badge&logo=html5&logoColor=white)  
+![CSS](https://img.shields.io/badge/CSS-1572B6?style=for-the-badge&logo=css3&logoColor=white)  
+![Vue](https://img.shields.io/badge/Vue-4FC08D?style=for-the-badge&logo=vue.js&logoColor=white)  
+![Astro](https://img.shields.io/badge/Astro-FF5D01?style=for-the-badge&logo=astro&logoColor=white)  
+![Tailwind](https://img.shields.io/badge/Tailwind-06B6D4?style=for-the-badge&logo=tailwindcss&logoColor=white)  
+
+**🛠️ Backend:**  
+![Python](https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white)  
+![Golang](https://img.shields.io/badge/Go-00ADD8?style=for-the-badge&logo=go&logoColor=white)  
+![Flask](https://img.shields.io/badge/Flask-000000?style=for-the-badge&logo=flask&logoColor=white)  
+![Hono](https://img.shields.io/badge/Hono-FF3A35?style=for-the-badge&logo=hono&logoColor=white)  
+
+**📦 Databases:**  
+![MySQL](https://img.shields.io/badge/MySQL-4479A1?style=for-the-badge&logo=mysql&logoColor=white)  
+![SQLite](https://img.shields.io/badge/SQLite-003B57?style=for-the-badge&logo=sqlite&logoColor=white)  
+![Redis](https://img.shields.io/badge/Redis-DC382D?style=for-the-badge&logo=redis&logoColor=white)  
+
+**💻 DevOps:**  
+![Git](https://img.shields.io/badge/Git-F05032?style=for-the-badge&logo=git&logoColor=white)  
+![Caddy](https://img.shields.io/badge/Caddy-2E3A2E?style=for-the-badge&logo=caddy&logoColor=white)  
+![Vagrant](https://img.shields.io/badge/Vagrant-1563FF?style=for-the-badge&logo=vagrant&logoColor=white)  
+
+### 🧰 Equipment  
+- **Laptop** - Asus E204YA
+  - **CPU** - AMD E2-7015
+  - **GPU** - AMD Radeon R2 (Stoney Ridge)
+  - **RAM** - 4 GB
+  - **Operating System** - Windows 10
+  - **Text Editor** - VSCodium, Notepad++
+  - **Browser** - Chrome, Brave  
+  - **Terminal** - Clink + Oh My Posh  
+
+### 📊 My Github Stats
+<p align="center">
+  <img src="https://github-readme-stats.vercel.app/api?username=indra87g&show_icons=true&theme=radical" alt="Indra's GitHub Stats" />
+</p>
+
+<p align="center">
+  <img src="https://github-readme-streak-stats.herokuapp.com/?user=indra87g&theme=radical" alt="GitHub Streak Stats" />
+</p>
+
+<a href="https://www.gitanimals.org/en_US?utm_medium=image&utm_source=indra87g&utm_content=line">
+  <img src="https://render.gitanimals.org/lines/indra87g?pet-id=671251643465219592" width="600" height="120" />
+</a>
+
+<p align="center">
+  <img src="https://github-profile-trophy.vercel.app/?username=indra87g&theme=radical&margin-w=10&rank=A,AA,AAA,S,SS,SSS,?&row=2&column=3" alt="Trophies" />
+</p>
+
+![Metrics](github-metrics.svg)
+
+![Visitors](https://visitor-badge.laobi.icu/badge?page_id=indra87g.indra87g)
+`
+
+console.log(readme)
